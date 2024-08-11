a77063f78f4c7ff94657f5e049720c29a76ef3fd
cssxsh <cssxsh@gmail.com>
2024-08-11 15:04:18 +0800

    update: dep

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
-    kotlin("jvm") version "1.7.22"
-    kotlin("plugin.jpa") version "1.7.22"
-    kotlin("plugin.serialization") version "1.7.22"
+    kotlin("jvm") version "1.8.22"
+    kotlin("plugin.jpa") version "1.8.22"
+    kotlin("plugin.serialization") version "1.8.22"
 
-    id("net.mamoe.mirai-console") version "2.13.2"
-    id("me.him188.maven-central-publish") version "1.0.0-dev-3"
+    id("net.mamoe.mirai-console") version "2.16.0"
+    id("me.him188.maven-central-publish") version "1.0.0"
 }
 
 group = "xyz.cssxsh.mirai"
@@ -26,20 +26,20 @@ repositories {
 }
 
 dependencies {
-    compileOnly("xyz.cssxsh.mirai:mirai-script-plugin:1.0.2")
-    compileOnly("xyz.cssxsh.mirai:mirai-hibernate-plugin:2.5.1")
+    compileOnly("xyz.cssxsh.mirai:mirai-script-plugin:1.1.0")
+    compileOnly("xyz.cssxsh.mirai:mirai-hibernate-plugin:2.8.2")
     testImplementation(kotlin("test"))
-    testImplementation("xyz.cssxsh.mirai:mirai-hibernate-plugin:2.5.1")
+    testImplementation("xyz.cssxsh.mirai:mirai-hibernate-plugin:2.8.2")
     testImplementation("org.luaj:luaj-jse:3.0.1")
-    testImplementation("org.graalvm.js:js:22.2.0")
-    testImplementation("org.graalvm.js:js-scriptengine:22.2.0")
+    testImplementation("org.graalvm.js:js:23.0.5")
+    testImplementation("org.graalvm.js:js-scriptengine:23.0.5")
     testImplementation("org.python:jython-standalone:2.7.3")
     //
-    implementation(platform("net.mamoe:mirai-bom:2.13.2"))
+    implementation(platform("net.mamoe:mirai-bom:2.16.0"))
     compileOnly("net.mamoe:mirai-console-compiler-common")
     testImplementation("net.mamoe:mirai-logging-slf4j")
     //
-    implementation(platform("org.slf4j:slf4j-parent:2.0.6"))
+    implementation(platform("org.slf4j:slf4j-parent:2.0.15"))
     testImplementation("org.slf4j:slf4j-simple")
 }
 
